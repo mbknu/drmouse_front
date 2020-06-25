28732b451a567fc9bad183d2c409616e09aa0f88
Bouchelot <bouchelot.m@gmail.com>
2020-06-25 11:13:52 +0200

    add timeline in homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HomeText from "./HomeText";
+import Timeline from "./Timeline/Timeline"
 
 const HomePage = () => {
     return (
         <>
             <HomeText />
+            <Timeline />
         </>
     );
 };
