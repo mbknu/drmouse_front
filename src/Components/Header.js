03d9cc3051a9554c86a5b03b77340b128f2a7c8c
David Faure <aswelldavid@gmail.com>
2020-06-25 09:13:38 +0200

    conflit resolved

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import logo from '../img/caremindwhite.svg';
-<<<<<<< HEAD
 import '../App.css';
-=======
-import "../App.css";
->>>>>>> a24992df6ec2ef6f75cc9b47962fd38e19e5492b
 
 const Header = () => {
     return (
